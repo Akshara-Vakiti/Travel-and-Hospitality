e7bb8d6ccc604357929a3e0960104966899ab643
Akshara-Vakiti <aksharavakiti96@gmail.com>
2025-04-12 20:55:52 +0530

    Update Flight Tickets.java

--- Flight Tickets.java
+++ Flight Tickets.java
@@ -3,5 +3,8 @@ public class FlightBooking
     public static void main (String[] args)
     {
       system.out.println("Flight is Booked");
+      system.out.println("Select the seat");
+      system.out.println("pick a date");
+      
     }
   }
